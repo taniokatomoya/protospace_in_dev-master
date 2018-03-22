32643366c71ee75d0e6660200ea17f64a8408e7a
東條晃多 <tjkota1090@icloud.com>
2018-03-22 11:18:53 +0900

    Create comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  before_action :set_prototype
+  def create
+    @comment = @prototype.comments.new(comment_params)
+    if @comment.save
+      respond_to do |format|
+        format.html {redirect_to prototype_path(params[:prototype_id]) }
+        format.json
+      end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy if @comment.user.id = current_user.id
+    redirect_to prototype_path(@prototype)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(prototype_id: params[:prototype_id], user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+end
