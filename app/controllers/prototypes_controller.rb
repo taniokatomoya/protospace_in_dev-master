74559da25411fa48b09b6dae133d93127a2937bc
渥美佑佳 <imusta99ym@gmail.com>
2018-03-23 09:37:19 +0900

    Fix_Indent
    Endのインデントを揃えた。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,7 +16,7 @@ class PrototypesController < ApplicationController
       redirect_to :root, notice: 'New prototype was successfully created'
     else
       redirect_to :root, alert: 'New prototype was unsuccessfully created'
-     end
+    end
   end
 
   def show
